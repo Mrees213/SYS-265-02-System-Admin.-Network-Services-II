4fb203ce11da1ec6ef992edd2db2ff66621b8661
Mrees213 <morgan.rees@mymail.champlain.edu>
2025-02-15 15:34:28 -0500

    secure-ssh.sh

---
+++ linux/web01/secure-ssh.sh
@@ -0,0 +1,7 @@
+#secure-ssh.sh
+#author Mrees213
+#creates a new ssh user from $1 parameter
+#adds a public key from the local repo or culred from the remote repo
+#removes roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
+
